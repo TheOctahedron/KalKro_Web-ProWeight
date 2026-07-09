8956f1d8a817f4704142ee7ab17871e8a1ac924f
Octahedron <256938541+TheOctahedron@users.noreply.github.com>
2026-07-09 18:09:56 +0300

    UPD: Apps&Games Division, Boring Calculator added
    I split the programs folder and added the boring calculator to the apps.
    
    - (backend\app\programs) two folders have been added: apps (responsible for system applications), games (responsible for installed programs).
    - (backend\app\programs\apps\boring_calculator.py) is a new file embedded in KalKro.

---
+++ backend/app/programs/apps/boring_calculator.py
@@ -0,0 +1,29 @@
+
+def boring_plus(numbers):
+    for number_one, number_two in numbers:
+        answer = number_one + number_two
+    return answer
+
+
+def boring_minus(numbers):
+    for number_one, number_two in numbers:
+        answer = number_one - number_two
+    return answer
+
+
+def boring_division(numbers):
+    for number_one, number_two in numbers:
+        answer = number_one / number_two
+    return answer
+
+
+def boring_multiplication(numbers):
+    for number_one, number_two in numbers:
+        answer = number_one * number_two
+    return answer
+
+
+def boring_plus(numbers):
+    for number_one, number_two in numbers:
+        answer = number_one * number_two * number_two / number_one * number_two / number_one + 913 / number_two * number_one * number_two * 32
+    return answer
---
+++ backend/app/programs/apps/randomizer.py
@@ -0,0 +1,5 @@
+import random
+
+def random_number(number):
+    answer = random.randint(1, number)
+    return (f"\n\nRandomizer: your result: \n\n{answer}")
